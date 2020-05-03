ad7d70701bf7a0e3b09fef7a8b4973c9682aa8bc
vgaudino <gaudinobello.vane@gmail.com>
2020-05-03 15:07:49 -0300

    My subbmission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##this function creates the vector that containg a cache of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    z <- NULL
+    set <- function(y) {
+        x <<- y
+        z <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) z <<- inverse 
+    getinv <- function() z
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+##Now with this function I can calculte with Solve function the inverse of the data vector from makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    z <- x$getinv()
+    if(!is.null(z)) {
+        message("getting cached data")
+        return(z)
+    }
+    data <- x$get()
+    z <- solve(data, ...)
+    x$setinv(z)
+    ## Return a matrix that is the inverse of 'x'
+    z
 }
+
